2cf36c772d4baed11efb1885efa67ee7fc2051a0
TinevimboMusingadi <tine2musi1@gmail.com>
2026-04-07 12:48:57 +0200

    Add masked MAPE metric utility

---
+++ utils/metrics.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+def masked_mape(y_true, y_pred, epsilon=0.1):
+    """
+    MAPE ignoring near-zero rainfall values to avoid division by zero.
+    """
+    mask = tf.abs(y_true) > epsilon
+    y_true_masked = tf.boolean_mask(y_true, mask)
+    y_pred_masked = tf.boolean_mask(y_pred, mask)
+    return tf.reduce_mean(tf.abs((y_true_masked - y_pred_masked) / (y_true_masked + epsilon))) * 100.0
