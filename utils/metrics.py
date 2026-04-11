0b1ab0ce8c04040ff132367a883a6a2751426d48
TinevimboMusingadi <tine2musi1@gmail.com>
2026-04-11 11:08:20 +0200

    Fix registration API compatibility for older Keras versions

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
-@tf.keras.saving.register_keras_serializable(package="CustomMetrics")
+@tf.keras.utils.register_keras_serializable(package="CustomMetrics")
 def masked_mape(y_true, y_pred, epsilon=0.1):
     """
     MAPE ignoring near-zero rainfall values to avoid division by zero.
